1a54b470c3b0a15a5a779b865552c27e0496f974
unknown <pooja.mutadak@thinkitive.com>
2021-07-07 17:08:56 +0530

    My first action is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 
 
-pwsh /home/Blazemeter-run.ps1 -apikey asas -apisecret asas -testidinput asas -showtaillog 'true' 
+pwsh /github/workspace/Blazemeter-run.ps1 -apikey asas -apisecret asas -testidinput asas -showtaillog 'true' 
 
 #RUN ["chmod", "+x", "pwsh /Blazemeter-run.ps1 -apikey asas -apisecret asas -testidinput asas -showtaillog 'true' "]
